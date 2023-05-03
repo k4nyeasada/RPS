2e961a0a53221cf0abd2afb866cf734a558b6a78
k4nyeasada <125545652+k4nyeasada@users.noreply.github.com>
2023-05-03 00:04:08 -0700

    Update CoinFlipGame.java

--- CoinFlipGame.java
+++ CoinFlipGame.java
@@ -2,43 +2,59 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class CoinFlipGame {
-    public static void main(String[] args) {
-        // Create a new Scanner object to read user input from the console
-        Scanner input = new Scanner(System.in);
-        // Create a new Random object to generate a random number for the coin flip
-        Random rand = new Random();
 
-        // Print a welcome message to the console
+    // Define a Scanner object and a Random object as instance variables
+    private Scanner input;
+    private Random rand;
+    private String player1Choice;
+    private int coin;
+
+    // Define a constructor to initialize the instance variables
+    public CoinFlipGame() {
+        input = new Scanner(System.in);
+        rand = new Random();
+    }
+
+    // Define a method to run game
+    public void runGame() {
+        printWelcomeMessage();
+        getPlayer1Choice();
+        flipCoin();
+        printWinner();
+    }
+    // Print a message to prompt the user to begin the game
+    private void printWelcomeMessage() {
         System.out.println("Welcome to the Coin Flip Game!");
-
-        // Print a message to prompt the user to begin the game
         System.out.println("Let's begin the game!\n");
+    }
 
-        // Declare a variable to store player 1's choice
-        String player1Choice;
-        // Loop until player 1 enters a valid choice of "heads" or "tails"
+    private void getPlayer1Choice() {
         do {
             // Prompt player 1 to enter their choice of "heads" or "tails"
             System.out.print("Player 1, enter your choice (heads/tails): ");
             // Read the user's input and convert it to lowercase for case-insensitive comparison
             player1Choice = input.nextLine().toLowerCase();
-            // Check if the user's input is invalid (not "heads" or "tails")
             if (!player1Choice.equals("heads") && !player1Choice.equals("tails")) {
-                // Print an error message to the console
                 System.out.println("Invalid response! Please choose between heads or tails.");
             }
-        // Continue looping until player 1 enters a valid choice
         } while (!player1Choice.equals("heads") && !player1Choice.equals("tails"));
+    }
 
-        // Generate a random number (0 or 1) to simulate the coin flip
-        int coin = rand.nextInt(2);
-        // Check if the coin landed on "heads" and player 1 chose "heads", or if the coin landed on "tails" and player 1 chose "tails"
+    private void flipCoin() {
+        coin = rand.nextInt(2);
+    }
+
+    //Determine winner based on results of coin flip
+    private void printWinner() {
         if ((coin == 0 && player1Choice.equals("heads")) || (coin == 1 && player1Choice.equals("tails"))) {
-            // Print a message to indicate that player 1 won the game
             System.out.println("Player 1 wins!");
         } else {
-            // Otherwise, print a message to indicate that player 2 won the game
             System.out.println("Player 2 wins!");
         }
+    }
+    
+    public static void main(String[] args) {
+        CoinFlipGame game = new CoinFlipGame();
+        game.runGame();
     }
 }
