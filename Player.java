e37bd7b6ed78e3411de1b632f5c52198de414f28
k4nyeasada <125545652+k4nyeasada@users.noreply.github.com>
2023-05-02 22:29:26 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -12,34 +12,42 @@ class Player {
         roundsWon = 0;
     }
     
+    //player name entry
     public String getName() {
         return name;
     }
     
+    //player choice entry
     public String getChoice() {
         return choice;
     }
     
+    //get RoundsWon
     public int getRoundsWon() {
         return roundsWon;
     }
     
+    //set player name
     public void setName(String n) {
         name = n.toLowerCase();
     }
     
+    //set player choice
     public void setChoice(String c) {
         choice = c.toLowerCase();
     }
     
+    //set roundswon
     public void setRoundsWon(int rw) {
         roundsWon = rw;
     }
     
+    //increment roundswon
     public void incrementRoundsWon() {
         roundsWon++;
     }
     
+    //get UserInput from console
     public String getUserInput(Scanner in, String msg) {
         System.out.print(msg);
         try {
